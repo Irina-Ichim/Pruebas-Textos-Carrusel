433595d24f8db4f6559f320cf1f6376ce02ab6ab
Gilda Irina Ichim <onlyirina7@gmail.com>
2023-09-30 01:18:50 +0200

    biblioteca Masonry.js para organizar las imágenes en un estilo de mosaico y añadiremos animaciones

---
+++ script2.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+    // Inicializar Masonry
+    $('.masonry').masonry({
+      itemSelector: '.masonry-item',
+      columnWidth: '.masonry-item',
+      percentPosition: true
+    });
+  });
+  
