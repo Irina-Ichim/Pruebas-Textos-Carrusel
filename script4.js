2185e73f7eb55e7b0ae0774035d7f9dfbe01b828
Gilda Irina Ichim <onlyirina7@gmail.com>
2023-10-12 01:34:06 +0200

    creando y el efecto scroll de texto para mostrar menos

--- script4.js
+++ script4.js
@@ -2,16 +2,41 @@
 
 document.addEventListener('DOMContentLoaded', function() {
   var parrafos = document.querySelectorAll('.parrafo');
+  var mostrarMasBtn = document.querySelector('.mostrar-mas');
+  var mostrarMenosBtn = document.querySelector('.mostrar-menos');
   var indiceActual = 0;
 
   window.mostrarSiguiente = function() {
     if (indiceActual < parrafos.length) {
       parrafos[indiceActual].classList.add('mostrar');
       indiceActual++;
-      console.log("Función mostrarSiguiente llamada");
-    } else {
-      document.querySelector('.mostrar-mas').style.display = 'none';
+
+      if (indiceActual === parrafos.length) {
+        // Todos los párrafos están mostrados, ocultar "Mostrar más"
+        mostrarMasBtn.style.display = 'none';
+        mostrarMenosBtn.style.display = 'inline-block';
+      }
     }
   };
+
+  window.mostrarMenos = function() {
+    // Ocultar todos los párrafos y mostrar solo el primero
+    parrafos.forEach(function(parrafo, index) {
+      parrafo.classList.remove('mostrar');
+      if (index === 0) {
+        parrafo.classList.add('mostrar');
+      }
+    });
+
+    // Reiniciar el índice para "Mostrar más"
+    indiceActual = 1;
+
+    // Mostrar "Mostrar más" y ocultar "Mostrar menos"
+    mostrarMasBtn.style.display = 'inline-block';
+    mostrarMenosBtn.style.display = 'none';
+  };
+
+  // Mostrar solo el primer párrafo al cargar la página
+  mostrarMenos();
 });
 
