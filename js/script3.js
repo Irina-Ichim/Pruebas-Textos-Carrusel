b1639a04239d3f05e783e7bb14b8f92a637797b9
Irina Ichim <onlyirina7@gmail.com>
2024-04-13 03:31:12 +0200

    agregando galeria imagenes

--- js/script3.js
+++ js/script3.js
@@ -1,17 +1,18 @@
-/*Cambiando color de texto, tambien utilizalo en index.html*/
 $(document).ready(function() {
-    var parrafos = $('.parrafo');
-    var indiceActual = 0;
-  
-    function mostrarSiguiente() {
-      if (indiceActual < parrafos.length) {
-        $(parrafos[indiceActual]).fadeIn().css('color', '#de43ba'); // Cambiado a fadeIn y color
-        indiceActual++;
-      } else {
-        $('.mostrar-mas').hide();
-      }
+  let parrafos = $('.parrafo');
+  let indiceActual = 0;
+
+  function mostrarSiguiente() {
+    if (indiceActual < parrafos.length) {
+      $(parrafos[indiceActual]).fadeIn().css('color', '#de43ba'); // Cambiado a fadeIn y color
+      indiceActual++;
+    } else {
+      $('.mostrar-mas').hide();
     }
+  }
+
+  $('.mostrar-mas').on('click', mostrarSiguiente);
+
   
-    $('.mostrar-mas').on('click', mostrarSiguiente);
   });
   
